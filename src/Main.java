fef492199ffde290982c365065664dcb09c3b6f4
Dmitriy Stroganov <Distrog@gmail.com>
2023-12-18 19:51:40 +0700

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 8");
+        int monthSavings = 29000;
+        int potTotalSavings = 0;
+        for (int i = 1; i <= 12; i++) {
+            potTotalSavings += monthSavings;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + potTotalSavings + " рублей");
+        }
+
+        System.out.println("Задача 9");
+        float bankTotalSavings = 0;
+        for (int i = 1; i <= 12; i++) {
+            bankTotalSavings *= 1.1f;
+            bankTotalSavings += monthSavings;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + bankTotalSavings + " рублей");
+        }
+
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            int result = 2 * i;
+            System.out.println("2*" + i + "=" + result);
+        }
     }
 }
